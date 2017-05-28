509dbdc23a32040c72f1cc47a0ea7a57d3a03e4a
Jason <rhuidean06@gmail.com>
2017-05-27 19:06:14 -0500

    revised aggregations

--- aggregations.py
+++ aggregations.py
@@ -1,3 +1,10 @@
 import numpy as np
+
+### Generate 100 random numbers between 0 and 1
 L = np.random.random(100)
-sum(L)
+print type(L)
+print "{} {}".format(sum(L),np.sum(L))
+
+### Compare the compile timing
+big_array = np.random.rand(1000000)
+print big_array[0]
