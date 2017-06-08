6336d67c03eedfb0434ac01beb778da24f45944a
Jason <rhuidean06@gmail.com>
2017-06-07 23:44:45 -0500

    modified numpy basics

--- basics_numpy.py
+++ basics_numpy.py
@@ -4,3 +4,12 @@ print "{} {}".format(type(L[0]),type(L2[0]))
 
 L3 = [True,"2",3.0,4]
 print [type(item) for item in L3]
+
+import numpy as np
+
+# integer array:
+print np.array([1,4,2,5,3])
+print np.array([1,4,2,5,3],dtype='float32')
+
+# multi-dimensional array vs list one-dimensional array
+print np.array([range(i,i+3) for i in [2,4,6]])
