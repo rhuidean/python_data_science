f8a9f64afee60f9669d025c582e00abc2a726e1f
Jason <rhuidean06@gmail.com>
2017-06-07 23:50:53 -0500

    modified pandas basics

--- pandas_basics.py
+++ pandas_basics.py
@@ -7,3 +7,9 @@ print data.index
 
 '''Series with explicit index'''
 data = pd.Series([0.25,0.5,0.75,1.0], index=['a','b','c','c'])
+print data
+
+'''Series with dictionary'''
+population_dict = {'CA':1234,'TX':4566,'NY':1429}
+population = pd.Series(population_dict)
+print "{} {} {}".format(population,population['CA'],population['CA':'NY'])
