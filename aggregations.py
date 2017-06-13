1add56f68a50dc9a77cdf1d0be864b8b694747af
Jason <rhuidean06@gmail.com>
2017-06-12 23:13:36 -0500

    add changed aggregations file

--- aggregations.py
+++ aggregations.py
@@ -52,7 +52,9 @@ def avg_medal_count():
 gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
 silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
 bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
-df=DataFrame({'gold':gold,'silver':silver,'bronze':bronze})
-points=[4,2,1]
+# df=DataFrame({'gold':gold,'silver':silver,'bronze':bronze})
+# points=[4,2,1]
     
-print np.dot(df,points)
+# print np.dot(df,points)
+
+
