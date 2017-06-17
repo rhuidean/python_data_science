7672fda682fbe9e11495e0f4945d8361e2f678e5
Jason <rhuidean06@gmail.com>
2017-06-16 23:40:28 -0500

    add compilation measurement for reciprocal ufunc

--- universal_function.py
+++ universal_function.py
@@ -2,7 +2,13 @@ import numpy as np
 np.random.seed(0)
 
 def compute_reciprocals(values):
+	### create a output bucket
 	output = np.empty(len(values))
+
+	### populate the bucket np array
 	for i in range(len(values)):
 		output[i] = 1.0/ values[i]
 	return output
+
+values = np.random.randint(1,10,size=5)
+print compute_reciprocals(values)
